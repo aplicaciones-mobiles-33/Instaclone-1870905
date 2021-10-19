95ed3994bf31982f5e14c1f82d21f81447f0c089
Melanie González <melanie.gonzalezgar@uanl.edu.mx>
2021-10-19 00:15:01 -0500

    Clase 12 Oct

--- src/app/publicacion/publicacion.component.ts
+++ src/app/publicacion/publicacion.component.ts
@@ -2,7 +2,9 @@ import { IonicModule } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
-import * as data from '../../assets/feed.json';
+
+import {HttpClient} from '@angular/common/http';
+//import * as data from '../../assets/feed.json';
 
 @Component({
   selector: 'app-publicacion',
@@ -10,36 +12,43 @@ import * as data from '../../assets/feed.json';
   styleUrls: ['./publicacion.component.scss'],
 })
 
+
 export class PublicacionComponent implements OnInit {
-  _filtrarPublicacion: number; //filtro de publicaciones en el perfil
-  publicacion: any;
-  usuario: any;
-  datos: any = data;
-  publicaciones: any = this.datos.publicaciones;
-  detallePublicacion: any;
 
-  get filtrarPublicacion(): number {
-    return this._filtrarPublicacion;
-  }
+  //_filtrarPublicacion: number; //filtro de publicaciones en el perfil
+  //publicacion: any;
+  //usuario: any;
   
-  set filtrarPublicacion(valor: number) {
-    console.log(valor);
-    this._filtrarPublicacion = valor;
-    this.detallePublicacion = this.cargarDetallePublicacion(valor);
-  }
+  publicacionId: any;
 
-  cargarDetallePublicacion(idPublicacion: number):any {
-     return this.publicaciones.find((publicacion: any) => publicacion.id );
-  }
+  //datos: any = data;
+  //publicaciones: any = this.datos.publicaciones;
+  //detallePublicacion: any;
+
   volver(): void {
     this._location.back();
   }
   constructor(private RutaActiva: ActivatedRoute, private _location : Location){}
-
+  
   ngOnInit() {
-    this._filtrarPublicacion = this.RutaActiva.snapshot.params.publicacionId;
-    console.log(this._filtrarPublicacion);
-    this.detallePublicacion =  this.cargarDetallePublicacion(this._filtrarPublicacion);
+    this.RutaActiva.queryParams.subscribe(params => {
+      console.log(params);
+      this.publicacionId = params.publicacionId;
+      console.log(this.publicacionId);
+    }
+    )
+
+    /*
+    
+    this.RutaActiva.paramMap.subscribe((params: ParamMap) => {
+      console.log(params);
+      this.publicacionId = +params.get('publicacionId');
+      console.log(this.publicacionId);
+    })
+    
+    */
+  
+    
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,25 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
 
 export const environment = {
-  production: false
+  production: false, 
+  firebaseConfig : {
+    apiKey: "AIzaSyDcMueHAXP4ofAjiV9CcR4JEj1JiPC9iyw",
+    authDomain: "insta-clone-app-8f063.firebaseapp.com",
+    projectId: "insta-clone-app-8f063",
+    storageBucket: "insta-clone-app-8f063.appspot.com",
+    messagingSenderId: "538442410886",
+    appId: "1:538442410886:web:ea70d1613371a0adbd5a07"
+  }
 };
 
 /*
